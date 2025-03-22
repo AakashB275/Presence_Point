3640136a1c14e5265885739d3d1a15e6298fc849
Sagar Jadhav <jadhavsagar1505@gmail.com>
2025-03-22 20:39:34 +0530

    main.dart fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:presence_point_2/pages/get_started.dart';
 import 'package:presence_point_2/pages/home_page.dart';
 import 'package:presence_point_2/pages/leaves.dart';
+import 'package:presence_point_2/pages/login.dart';
 import 'package:presence_point_2/wrapper.dart';
-=======
-import 'package:presence_point_2/pages/home_page.dart';
->>>>>>> 936c27c17eb2f91e7748d1d2ddfb3dd1a79a88c4
 import 'package:supabase_flutter/supabase_flutter.dart';
-import './pages/login.dart';
 import './pages/analytics_page.dart';
 import './pages/register.dart';
 import './pages/profile.dart';
@@ -30,24 +26,17 @@ Future<void> main() async {
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
     routes: {
-<<<<<<< HEAD
       '/': (context) => Wrapper(),
-=======
-      '/': (context) => HomePage(),
->>>>>>> 936c27c17eb2f91e7748d1d2ddfb3dd1a79a88c4
-      '/login': (context) => LoginPage(),
       '/home': (context) => HomePage(),
-      '/register': (context) => RegisterScreen(),
-      '/profile': (context) => ProfileScreen(),
       '/analytics': (context) => AnalyticsPage(),
-      '/neworganisation': (context) => NewOrganisation(),
+      '/login': (context) => LoginPage(),
+      '/register': (context) => RegisterScreen(),
       '/organisationdetails': (context) => OrganisationDetails(),
-<<<<<<< HEAD
       '/leave': (context) => LeavesScreen(),
-=======
       '/usercheckin': (context) => UserCheckin(),
       '/geofencingscreen': (context) => GeofencingMapScreen(),
->>>>>>> 936c27c17eb2f91e7748d1d2ddfb3dd1a79a88c4
+      '/neworganisation': (context) => NewOrganisation(),
+      '/profile': (context) => ProfileScreen(),
     },
   ));
 }
